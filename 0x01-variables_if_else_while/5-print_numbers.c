8f94a355cf6e74f4bc14f8868c94bdcb1281b1e0
emmanueldavids <emmanueldavids417@gmail.com>
2022-10-17 16:43:46 -0400

    Print Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,7 +4,7 @@
 /**
  * main - Entry point
  *
- * Description: This program prints all single digits of base 10 startubg from 0
+ * Description: This program print all single digits of base 10 startubg from 0
  *
  * Return: Always 0 for success
  *
@@ -12,10 +12,11 @@
 
 int main(void)
 {
-  int b10;
-  for(b10 = 0; b10 < 10; b10++){
-    printf("%d", b10);
-  }
-  printf("\n");
-  return (0);
+int b10;
+for (b10 = 0; b10 < 10; b10++)
+{
+printf("%d", b10);
+}
+printf("\n");
+return (0);
 }
