2cc3dff01cf85a92ca4f263ae3a5b0c80c6689e0
emmanueldavids <emmanueldavids417@gmail.com>
2022-10-16 22:58:24 -0400

    Print Alhpabets Uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Description: A program that prints alphaBETS in both lower and upper case
+ *
+ * Return: Always 0 for success
+ *
+ */
+
+int main(void)
+{
+  char ch;
+  for(ch = 'a'; ch <= 'z'; ch++)
+  {
+    putchar(ch);
+  }
+
+  for(ch = 'A'; ch <= 'Z'; ch++)
+  {
+    putchar(ch);
+  }
+  putchar('\n');
+  return (0);
+}
