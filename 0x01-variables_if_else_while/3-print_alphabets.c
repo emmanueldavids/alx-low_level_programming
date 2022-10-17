45b2d6951e703e20faa8c41ba3105c5478efc1ad
emmanueldavids <emmanueldavids417@gmail.com>
2022-10-17 16:49:53 -0400

    Print Alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,16 +12,16 @@
 
 int main(void)
 {
-  char ch;
-  for(ch = 'a'; ch <= 'z'; ch++)
-  {
-    putchar(ch);
-  }
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
 
-  for(ch = 'A'; ch <= 'Z'; ch++)
-  {
-    putchar(ch);
-  }
-  putchar('\n');
-  return (0);
+for (ch = 'A'; ch <= 'Z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
 }
