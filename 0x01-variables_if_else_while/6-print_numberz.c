831c3dc1e3fb80efc00039e8eda474da5548d6d8
emmanueldavids <emmanueldavids417@gmail.com>
2022-10-16 23:27:36 -0400

    Print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Desscription: using putchar to handle integer values
+ *
+ * Return: return 0 for Success
+ */
+int main(void){
+  int b10;
+  for(b10 = '0'; b10 <= '9'; b10++){
+    putchar(b10);
+  }
+  putchar('\n');
+  return (0);
+}
