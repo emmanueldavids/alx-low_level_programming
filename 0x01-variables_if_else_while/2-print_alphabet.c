4f2ef82b561e3433fb0cd4d1471a8b2457f28616
emmanueldavids <emmanueldavids417@gmail.com>
2022-10-17 16:52:17 -0400

    Print Alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,7 +12,7 @@ int main(void)
 {
 char lowercase;
 
-for(lowercase ='a'; lowercase <= 'z'; lowercase++)
+for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
 putchar(lowercase);
 putchar('\n');
 return (0);
