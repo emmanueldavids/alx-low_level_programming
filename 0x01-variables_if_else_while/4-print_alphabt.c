f8fd574bdf923991525135c57c561e2d0ebdd146
emmanueldavids <emmanueldavids417@gmail.com>
2022-10-17 16:46:07 -0400

    Print Alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,18 +12,18 @@
 
 int main(void)
 {
-  char ch, e, q;
+char ch, e, q;
 
-  e = 'e';
-  q = 'q';
+e = 'e';
+q = 'q';
 
-  for(ch = 'a'; ch <= 'z'; ch++)
-  {
-    if(ch != e && ch != q){
-      putchar(ch);
-    }
-  }
-  putchar('\n');
-  return (0);
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch != e && ch != q)
+{
+putchar(ch);
 }
-
+}
+putchar('\n');
+return (0);
+}
