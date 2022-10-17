260b2545bd8b31b8403b8b68f810da87904769b7
emmanueldavids <emmanueldavids417@gmail.com>
2022-10-17 16:15:45 -0400

    Print Comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,24 +10,24 @@
 
 int main(void)
 {
-  int d, p;
-  for (d = '0'; d < '9'; d++)
-  {
-    for (p = d + 1; p <= '9'; p++)
-    {
-      if(p != d)
-      {
-        putchar(d);
-        putchar(p);
+int d, p;
+for (d = '0'; d < '9'; d++)
+{
+for (p = d + 1; p <= '9'; p++)
+{
+if(p != d)
+{
+putchar(d);
+putchar(p);
 
-        if(d =='8' && p == '9')
-          continue;
-        putchar(',');
-        putchar(' ');
-      }
-    }
-  }
-  putchar('\n');
+if(d =='8' && p == '9')
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
 
-  return (0);
+return (0);
 }
