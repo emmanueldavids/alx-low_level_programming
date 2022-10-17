0bdd1fe894329740081bdcdba37f5446cbc9f7bd
emmanueldavids <emmanueldavids417@gmail.com>
2022-10-16 21:56:11 -0400

    Print Alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 for success
+ *
+ */
+
+int main(void)
+{
+  char lowercase;
+
+  for (lowercase='a'; lowercase <='z'; lowercase++)
+    putchar(lowercase);
+  putchar('\n');
+  return (0);
+}
