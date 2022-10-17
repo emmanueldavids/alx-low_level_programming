c64995a675424d627256c4631fa1b0cfdad22813
emmanueldavids <emmanueldavids417@gmail.com>
2022-10-17 16:28:33 -0400

    Print Base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,16 +8,18 @@
  *
  * Return: Always 0 for success.
  */
-int main(void){
-  char ch;
-  int b16;
+int main(void)
+{
+char ch;
+int b16;
 
-  for(b16 = '0'; b16 <= '9'; b16++){
-    putchar(b16);
-  }
-  for(ch = 'a'; ch <= 'f'; ch++){
-    putchar(ch);
-  }
-  putchar('\n');
-  return (0);
+for (b16 = '0'; b16 <= '9'; b16++){
+putchar(b16);
+}
+for (ch = 'a'; ch <= 'f'; ch++) 
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
 }
