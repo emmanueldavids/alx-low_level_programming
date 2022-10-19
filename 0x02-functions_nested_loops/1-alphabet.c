f986fb42b5543cc2cfa7039e274eb63e98f10248
emmanueldavids <emmanueldavids417@gmail.com>
2022-10-19 15:02:43 -0400

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+char world;
+for (world = 'a'; world <= 'z'; world++)
+{
+_putchar(world);
+}
+_putchar('\n');
+}
