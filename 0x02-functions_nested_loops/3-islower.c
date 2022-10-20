5158d20a726cdd06cd52ca2a006a7fd6e11cf416
emmanueldavids <emmanueldavids417@gmail.com>
2022-10-20 03:30:36 -0400

    Is lower case

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,9 +2,9 @@
 
 /**
  * _islower - function name
- *
+ * @c: An input character
  * Description: function uses _putchar function to print
- *
+ * alphabet in lowercase 10 times
  * Return: 1 if is lowercase or 0 if is uppercase.
  */
 int _islower(int c)
