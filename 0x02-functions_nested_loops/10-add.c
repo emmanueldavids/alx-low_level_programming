d7eaa202f57338b29b818227ecbc74b911cf5303
emmanueldavids <emmanueldavids417@gmail.com>
2022-10-20 01:47:01 -0400

    Add 2 integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* add - check the code
+* @a: the integers as argc and argv
+* @b: the integers as argc
+* Return: Always 0.
+*/
+
+int add(int a, int b)
+{
+int sum;
+sum = a + b;
+
+return (sum);
+}
