df4091fecc6a453a4115e076696cce39a0cc5d57
emmanueldavids <emmanueldavids417@gmail.com>
2022-10-20 03:38:32 -0400

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,8 +2,9 @@
 
 /**
 *_abs - Check Holberton
-*
-* Return: Absolut value of number r
+* @r: An integre input
+** Description: This function returns absolute value of a number
+** Return: Absolut value of number r
 **/
 int _abs(int r)
 {
