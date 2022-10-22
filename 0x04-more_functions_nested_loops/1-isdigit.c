e3f58a8f134e6af724a617fbc1f6b6c03b384146
emmanueldavids <emmanueldavids417@gmail.com>
2022-10-22 16:34:39 -0400

    Is Upper

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -7,17 +7,17 @@
  */
 int _isdigit(int c)
 {
-	char i = '0';
-	int isdigit = 0;
+char i = '0';
+int isdigit = 0;
 
-	for (; i <= '9'; i++)
-	{
-		if (i == c)
-		{
-			isdigit = 1;
-			break;
-		}
-	}
+for (; i <= '9'; i++)
+{
+if (i == c)
+{
+isdigit = 1;
+break;
+}
+}
 
-	return (isdigit);
+return (isdigit);
 }
