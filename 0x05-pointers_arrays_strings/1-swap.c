a2d36c7b73240bd9496566f7b2511ed1e71ca975
emmanueldavids <emmanueldavids417@gmail.com>
2022-10-25 00:48:38 -0400

    swap values of two

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - swap values of two nums
+ * @b : first pointer
+ * @a : second pointer
+ * Return: 0.
+ */
+void swap_int(int*a, int *b)
+{
+*a = 42;
+*b = 98;
+}
