2a57e1ce9b4728ad03a6b2c942e1ea5a247f454e
emmanueldavids <emmanueldavids417@gmail.com>
2022-10-27 15:55:46 -0400

    string concatenate

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,10 +10,26 @@
  * */
 char *_strncat(char *dest, char *src, int n)
 {
-int index = 0, dest_len = 0;
-while (dest[index++])
-dest_len++;
-for (index = 0; src[index] && index < n; index++)
-dest[dest_len++] = src[index];
-return (dest);
+int srclen = 0, i = 0;
+char *temp = dest, *start = src;
+
+while (*src)
+{
+srclen++;
+src++;
+}
+
+while (*dest)
+dest++;
+
+if (n > srclen)
+n = srclen;
+
+src = start;
+
+for (; i < n; i++)
+*dest++ = *src++;
+
+*dest = '\0';
+return (temp);
 }
