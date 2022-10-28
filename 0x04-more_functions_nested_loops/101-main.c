a7e397d5d8c1720a216ed688ab5928c49c9267b3
emmanueldavids <emmanueldavids417@gmail.com>
2022-10-28 13:43:12 -0400

    Printing an integer

--- 0x04-more_functions_nested_loops/101-main.c
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -9,15 +9,15 @@ int main(void)
 {
   print_numbers(98);
   _putchar('\n');
-  print_number(402);
+  print_numbers(402);
   _putchar('\n');
-  print_number(1024);
+  print_numbers(1024);
   _putchar('\n');
-  print_number(0);
+  print_numbers(0);
   _putchar('\n');
-  print_number(-98);
+  print_numbers(-98);
   _putchar('\n');
-  print_number(-987654321);
+  print_numbers(-987654321);
   _putchar('\n');
   return (0);
 }
