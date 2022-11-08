2d8ca590e702a9d53d575102b815f8c76f92183d
emmanueldavids <emmanueldavids417@gmail.com>
2022-11-07 21:18:01 -0500

    Concatenates 2 strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,11 +10,11 @@ int _strlen(char *s)
 {
 int counter = 0;
 if (!*s)
-        return (0);
+return (0);
 while (*s)
 {
-        counter++;
-        s++;
+counter++;
+s++;
 }
 return (counter);
 }
@@ -26,28 +26,28 @@ return (counter);
  */
 char *str_concat(char *s1, char *s2)
 {
-        char *new;
-        unsigned int i;
-        unsigned int j;
-        int total = 0;
-        if (!s1)
-                s1 = "";
-        if (!s2)
-                s2 = "";
-        total += _strlen(s1) + _strlen(s2);
-        new = malloc((total * sizeof(char)) + 1);
-        if (new == NULL)
-        {
-                return (NULL);
-        }
-        for (i = 0; s1[i]; i++)
-        {
-                new[i] = s1[i];
-        }
-        for (j = 0; s2[j]; j++, i++)
-        {
-                new[i] = s2[j];
-        }
-        new[i] = '\0';
-        return (new);
+char *new;
+unsigned int i;
+unsigned int j;
+int total = 0;
+if (!s1)
+s1 = "";
+if (!s2)
+s2 = "";
+total += _strlen(s1) + _strlen(s2);
+new = malloc((total * sizeof(char)) + 1);
+if (new == NULL)
+{
+return (NULL);
+}
+for (i = 0; s1[i]; i++)
+{
+new[i] = s1[i];
+}
+for (j = 0; s2[j]; j++, i++)
+{
+new[i] = s2[j];
+}
+new[i] = '\0';
+return (new);
 }
